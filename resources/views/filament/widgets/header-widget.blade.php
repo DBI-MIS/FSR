df3560886f1f3449e63f823021e7a8b9009e27d6
DBI-MIS <desktoppublisher@dbiphils.com>
2024-09-03 15:27:27 +0800

    update

--- resources/views/filament/widgets/header-widget.blade.php
+++ resources/views/filament/widgets/header-widget.blade.php
@@ -8,11 +8,22 @@
       
         <div class="w-full flex justify-between items-center gap-1 bg-gradient-to-br from-sky-600 to-sky-900 text-white rounded-md divide-x divide-white ">
             <div href="{{route('filament.admin.resources.fsrs.create')}}"
-            wire:navigate class="w-full text-nowrap text-center cursor-pointer">Add FSR</div>
+            wire:navigate class="w-full text-nowrap text-center cursor-pointer text-sm font-bold ">
+            <span>+</span>
+            <span>Add FSR</span>
+            </div>
             <div href="{{route('filament.admin.resources.projects.create')}}"
-            wire:navigate class="w-full text-nowrap text-center cursor-pointer">Add Project</div>
+            wire:navigate class="w-full text-nowrap text-center cursor-pointer text-sm font-bold">
+            <span>+</span>
+            <span>Add Project</span>
+            
+        </div>
             <div href="{{route('filament.admin.resources.dbe-personnels.create')}}"
-            wire:navigate class="w-full text-nowrap text-center cursor-pointer">Add DBE Personnel</div>
+            wire:navigate class="w-full text-nowrap text-center cursor-pointer text-sm font-bold">
+            <span>+</span>
+            <span>Add DBE Personnel</span>
+            
+        </div>
            
         
         </div>
